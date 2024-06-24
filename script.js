91a01ac53c840a65c06a810cdb63d314b505ef25
Matt <mcalabia@baunfire.com>
2024-06-25 05:30:12 +0800

    utm para

--- script.js
+++ script.js
@@ -13,27 +13,29 @@ $(document).ready(function() {
 });
 
 
-  var queryString = window.location.search;
-  console.log(queryString);
-  // ?utm_source=facebook&utm_medium=post&utm_campaign=webflow
-  var URLSearchParams_wb = new URLSearchParams(queryString);
+var queryString = window.location.search;
+console.log(queryString);
+// ?utm_source=facebook&utm_medium=post&utm_campaign=webflow&utm_content=content&utm_term=term&gclid=abcd1234
+var URLSearchParams_wb = new URLSearchParams(queryString);
 
-  const utmParameters = [
-    "utm_source", 
-    "utm_medium", 
-    "utm_campaign"
-  ];
+const utmParameters = [
+  "utm_source", 
+  "utm_medium", 
+  "utm_campaign",
+  "utm_content",
+  "utm_term",
+  "gclid"
+];
 
-  for (const utm_element of utmParameters) {
-    /* if utm_source exist */
-    $( "form" ).each(function( index ) {
-      if(URLSearchParams_wb.has(utm_element)){
-        console.log(utm_element + "is exist");
-        /* get UTM value of this utm param */
-        var value = URLSearchParams_wb.get(utm_element)
-        /* change form hidden feild to this utm url value */
-        $( this ).find("."+utm_element).val(value);
-      }
-
-    })
-  }/* end for loop */
+for (const utm_element of utmParameters) {
+  /* if the UTM parameter exists */
+  $("form").each(function(index) {
+    if (URLSearchParams_wb.has(utm_element)) {
+      console.log(utm_element + " exists");
+      /* get the UTM value of this UTM param */
+      var value = URLSearchParams_wb.get(utm_element);
+      /* change the form hidden field to this UTM URL value */
+      $(this).find("." + utm_element).val(value);
+    }
+  });
+}/* end for loop */
