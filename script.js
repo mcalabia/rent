9eadff670e222e358bfc723081a343ffcd8ed1b6
Matt <mcalabia@baunfire.com>
2024-06-25 05:43:25 +0800

    cookie utm

--- script.js
+++ script.js
@@ -13,9 +13,31 @@ $(document).ready(function() {
 });
 
 
+// Function to set a cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+// Function to get a cookie by name
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+// Get the query string from the URL
 var queryString = window.location.search;
-console.log(queryString);
-// ?utm_source=facebook&utm_medium=post&utm_campaign=webflow&utm_content=content&utm_term=term&gclid=abcd1234
 var URLSearchParams_wb = new URLSearchParams(queryString);
 
 const utmParameters = [
@@ -27,15 +49,22 @@ const utmParameters = [
   "gclid"
 ];
 
-for (const utm_element of utmParameters) {
-  /* if the UTM parameter exists */
-  $("form").each(function(index) {
-    if (URLSearchParams_wb.has(utm_element)) {
-      console.log(utm_element + " exists");
-      /* get the UTM value of this UTM param */
-      var value = URLSearchParams_wb.get(utm_element);
-      /* change the form hidden field to this UTM URL value */
-      $(this).find("." + utm_element).val(value);
+// Save UTM parameters and GCLID into cookies
+utmParameters.forEach(function(utm_element) {
+  if (URLSearchParams_wb.has(utm_element)) {
+    var value = URLSearchParams_wb.get(utm_element);
+    setCookie(utm_element, value, 30); // Save cookie for 30 days
+  }
+});
+
+// Auto-populate the form fields with cookie values
+$(document).ready(function() {
+  utmParameters.forEach(function(utm_element) {
+    var cookieValue = getCookie(utm_element);
+    if (cookieValue) {
+      $("form").each(function() {
+        $(this).find("." + utm_element).val(cookieValue);
+      });
     }
   });
-}/* end for loop */
+});
