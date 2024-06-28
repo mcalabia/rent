733f3ccc082cbaa9605a21f1b6feb085e0b4cf1f
Matt <mcalabia@baunfire.com>
2024-06-28 08:47:05 +0800

    data

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
   });
 
   $('#nav_search_btn').click(function() {
-    var query = $('#nav-search').val();
+    var query = $('.navbar_search_input').val();
     var url = 'https://redfin-group.webflow.io/search?query=' + encodeURIComponent(query);
     window.location.href = url;
 });
