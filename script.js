4153f4dc9b15ac6350803ac883e176f2b4fda216
Matt <mcalabia@baunfire.com>
2024-07-03 07:57:14 +0800

    script update

--- script.js
+++ script.js
@@ -28,50 +28,6 @@ $(document).ready(function() {
 });
 
 
-// Function to set a cookie
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-// Function to get a cookie by name
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-// Get the query string from the URL
-var queryString = window.location.search;
-var URLSearchParams_wb = new URLSearchParams(queryString);
-
-const utmParameters = [
-  "utm_source", 
-  "utm_medium", 
-  "utm_campaign",
-  "utm_content",
-  "utm_term",
-  "gclid"
-];
-
-// Save UTM parameters and GCLID into cookies
-utmParameters.forEach(function(utm_element) {
-  if (URLSearchParams_wb.has(utm_element)) {
-    var value = URLSearchParams_wb.get(utm_element);
-    setCookie(utm_element, value, 30); // Save cookie for 30 days
-  }
-});
-
 // Auto-populate the form fields with cookie values
 $(document).ready(function() {
   utmParameters.forEach(function(utm_element) {
