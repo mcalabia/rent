86a5cc9ef99b4242e53028a70391169ef9c21b65
Matt <mcalabia@baunfire.com>
2024-07-13 04:11:41 +0800

    sitemap

--- sitemap.js
+++ sitemap.js
@@ -15,7 +15,6 @@ document.addEventListener("DOMContentLoaded", function() {
           return;
         }
 
-        var rootList = document.createElement('ul');
         var urlMap = {};
 
         for (var i = 0; i < urls.length; i++) {
@@ -25,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
           var segments = relativeUrl.split('/').filter(Boolean);
           var pageTitle = segments.pop().replace(/-/g, ' ').replace('.html', ''); // Extract page title from relative URL
 
-          var parent = rootList;
+          var parent = pageListContainer;
           var path = '';
 
           segments.forEach(segment => {
@@ -48,21 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
           listItem.appendChild(pageLink);
           parent.appendChild(listItem);
-        }
-
-        // Remove the first parent if it has children
-        if (rootList.children.length === 1) {
-          var firstChild = rootList.children[0];
-          var grandChildren = firstChild.querySelector('ul');
-          if (grandChildren) {
-            var newRootList = document.createElement('ul');
-            for (var j = 0; j < grandChildren.children.length; j++) {
-              newRootList.appendChild(grandChildren.children[j].cloneNode(true));
-            }
-            pageListContainer.appendChild(newRootList);
-          }
-        } else {
-          pageListContainer.appendChild(rootList);
         }
       })
       .catch(error => console.error('Error fetching sitemap:', error));
