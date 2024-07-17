7edceb0d0b4bc76e83aeb4073408d37014e9acd5
Matt <mcalabia@baunfire.com>
2024-07-17 10:28:21 +0800

    site update

--- script.js
+++ script.js
@@ -220,3 +220,10 @@ keyFeaturesMobile();
 mobileNavSearch();
 
 });
+
+$(document).mouseup(function(e) {
+  var container = $(".get-started-form-slider"); 
+  if (!container.is(e.target) && container.has(e.target).length === 0) {        
+    container.hide();
+  }
+});
