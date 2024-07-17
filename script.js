f02136f77e0731d3cdbb4afdefebfeeb77d52611
Matt <mcalabia@baunfire.com>
2024-07-17 10:31:24 +0800

    script update

--- script.js
+++ script.js
@@ -225,5 +225,6 @@ $(document).mouseup(function(e) {
   var container = $(".get-started-form-slider"); 
   if (!container.is(e.target) && container.has(e.target).length === 0) {        
     container.hide();
+    $(".close-img").click();
   }
 });
