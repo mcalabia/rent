89829bdd23680da7bc7541b6b472a0b0f346c7be
Matt <mcalabia@baunfire.com>
2024-07-19 05:19:57 +0800

    copy

--- script.js
+++ script.js
@@ -237,6 +237,7 @@ function copyToClipboard(){
     $linkInput[0].setSelectionRange(0, 99999); // For mobile devices
     document.execCommand('copy');
     alert('Link copied to clipboard!');
+    console.log('Link copied to clipboard!');
 });
 }
 
