3c3b2a669953529cb2ced0a57b9e7d58ee00d3a9
Matt <mcalabia@baunfire.com>
2024-07-24 08:32:53 +0800

    gsap drag

--- script.js
+++ script.js
@@ -323,7 +323,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Initialize Draggable
   Draggable.create(".gallery-body", {
       type: "x",
-      bounds: document.body,
+      bounds: ".gallery-container",
       inertia: true,
       edgeResistance: 0.8,
       onDrag: function() {
